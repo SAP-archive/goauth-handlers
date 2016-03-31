c589224a6a5c3000b4adb560c6e43be663522551
Ivan Borshukov <ivan.borshukov@sap.com>
2016-03-31 13:08:42 +0300

    Add CallbackHandler

--- handlers.go
+++ handlers.go
@@ -107,3 +107,83 @@ func hasMissingScope(actual, expected []string) bool {
 	}
 	return len(m) != 0
 }
+
+type CallbackHandler struct {
+	Provider TokenProvider
+	Store    SessionStore
+}
+
+func (h *CallbackHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// TODO(ivan): why ignore err here?
+	session, _ := h.Store.Get(req, SessionName)
+
+	if errParam := req.FormValue("error"); errParam != "" {
+		session.Values = nil
+		session.Save(req, w)
+		switch errParam {
+		case "invalid_scope":
+			http.Error(w, "You do not have the required authorization.", http.StatusForbidden)
+		default:
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	clearSession := sessionCleaner(session, w, req)
+	state := req.FormValue("state")
+	if state == "" {
+		clearSession("Missing state query parameter.", http.StatusBadRequest)
+		return
+	}
+
+	code := req.FormValue("code")
+	if code == "" {
+		clearSession("Missing code query parameter.", http.StatusBadRequest)
+		return
+	}
+
+	// TODO(ivan): extract targetUrl as constant
+	targetURL, ok := session.Values["targetUrl"].(string)
+	if !ok {
+		clearSession("Missing redirect URL.", http.StatusBadRequest)
+		return
+	}
+	delete(session.Values, "targetUrl")
+
+	expectedState, ok := session.Values["state"].(string)
+	if !ok {
+		clearSession("Missing state.", http.StatusBadRequest)
+		return
+	}
+	delete(session.Values, "state")
+
+	if state != expectedState {
+		clearSession("Invalid state parameter.", http.StatusBadRequest)
+		return
+	}
+
+	token, err := h.Provider.RequestToken(code)
+	if err != nil {
+		clearSession("Could not retrieve token from provider.", http.StatusInternalServerError)
+		return
+	}
+
+	tokenBytes, err := json.Marshal(token)
+	if err != nil {
+		// should not happen
+		clearSession(http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		panic(err)
+	}
+
+	session.Values["token"] = string(tokenBytes)
+	session.Save(req, w)
+	http.Redirect(w, req, targetURL, http.StatusFound)
+}
+
+func sessionCleaner(s *sessions.Session, w http.ResponseWriter, req *http.Request) func(string, int) {
+	return func(error string, code int) {
+		s.Values = nil
+		s.Save(req, w)
+		http.Error(w, error, code)
+	}
+}
--- handlers_test.go
+++ handlers_test.go
@@ -195,3 +195,234 @@ var _ = Describe("Handler", func() {
 		})
 	})
 })
+
+var _ = Describe("CallbackHandler", func() {
+	const unsetResponseCode = -1
+	const originalURL = "http://some/resource/somewhere"
+	const oauthState = "SOME_STATE_VALUE"
+	const oauthCode = "SOME_OAUTH_CODE"
+
+	var oauthToken *oauth2.Token
+	var tokenProvider *fakes.FakeTokenProvider
+
+	var sessionStore *fakes.FakeSessionStore
+	var session *sessions.Session
+
+	var handler http.Handler
+
+	var request *http.Request
+	var response *httptest.ResponseRecorder
+
+	BeforeEach(func() {
+		sessionStore = new(fakes.FakeSessionStore)
+		session = sessions.NewSession(sessionStore, SessionName)
+		session.Values["targetUrl"] = originalURL
+		session.Values["state"] = oauthState
+		sessionStore.GetReturns(session, nil)
+
+		oauthToken = &oauth2.Token{
+			AccessToken:  "SomeAccessToken",
+			RefreshToken: "SomeRefreshToken",
+			TokenType:    "bearer",
+			Expiry:       time.Now().Add(time.Hour),
+		}
+		tokenProvider = new(fakes.FakeTokenProvider)
+		tokenProvider.RequestTokenReturns(oauthToken, nil)
+
+		handler = &CallbackHandler{
+			Provider: tokenProvider,
+			Store:    sessionStore,
+		}
+
+		var err error
+		request, err = http.NewRequest("GET", "http://some/resource", nil)
+		Ω(err).ShouldNot(HaveOccurred())
+		request.Form = make(map[string][]string)
+		request.Form.Add("state", oauthState)
+		request.Form.Add("code", oauthCode)
+
+		response = httptest.NewRecorder()
+		response.Code = unsetResponseCode
+	})
+
+	JustBeforeEach(func() {
+		handler.ServeHTTP(response, request)
+	})
+
+	Context("when everything works as expected", func() {
+		It("should get a session", func() {
+			Ω(sessionStore.GetCallCount()).Should(Equal(1))
+			argRequest, argName := sessionStore.GetArgsForCall(0)
+			Ω(argRequest).Should(Equal(request))
+			Ω(argName).Should(Equal(SessionName))
+		})
+
+		It("should remove state parameter from session", func() {
+			Ω(session.Values).ShouldNot(HaveKey("state"))
+		})
+
+		It("should remove original url from session", func() {
+			Ω(session.Values).ShouldNot(HaveKey("targetUrl"))
+		})
+
+		It("should request a token", func() {
+			Ω(tokenProvider.RequestTokenCallCount()).Should(Equal(1))
+			argCode := tokenProvider.RequestTokenArgsForCall(0)
+			Ω(argCode).Should(Equal(oauthCode))
+		})
+
+		It("should store token in session", func() {
+			tokenObj, exists := session.Values["token"]
+			Ω(exists).Should(BeTrue())
+			tokenString, isString := tokenObj.(string)
+			Ω(isString).Should(BeTrue())
+			var token oauth2.Token
+			err := json.Unmarshal([]byte(tokenString), &token)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(token).Should(Equal(*oauthToken))
+		})
+
+		It("should save the session", func() {
+			Ω(sessionStore.SaveCallCount()).Should(Equal(1))
+			argRequest, argResponse, argSession := sessionStore.SaveArgsForCall(0)
+			Ω(argRequest).Should(Equal(request))
+			Ω(argResponse).Should(Equal(response))
+			Ω(argSession).Should(Equal(session))
+		})
+
+		It("should redirect to original address", func() {
+			Ω(response.Code).Should(Equal(http.StatusFound))
+			Ω(response.Header().Get("Location")).Should(Equal(originalURL))
+		})
+	})
+
+	itShouldDeleteSession := func() {
+		It("should delete session", func() {
+			Ω(session.Values).Should(BeNil())
+			Ω(sessionStore.SaveCallCount()).Should(Equal(1))
+			argRequest, argResponse, argSession := sessionStore.SaveArgsForCall(0)
+			Ω(argRequest).Should(Equal(request))
+			Ω(argResponse).Should(Equal(response))
+			Ω(argSession).Should(Equal(session))
+		})
+	}
+
+	itShouldReturnBadRequest := func() {
+		It("should return bad request", func() {
+			Ω(response.Code).Should(Equal(http.StatusBadRequest))
+		})
+	}
+
+	itShouldReturnInternalServerError := func() {
+		It("should return bad request", func() {
+			Ω(response.Code).Should(Equal(http.StatusInternalServerError))
+		})
+	}
+
+	itShouldReturnForbidden := func() {
+		It("should return forbidden", func() {
+			Ω(response.Code).Should(Equal(http.StatusForbidden))
+		})
+	}
+
+	Context("when UAA responds with invalid_scope error param", func() {
+		BeforeEach(func() {
+			request.Form.Add("error", "invalid_scope")
+		})
+
+		itShouldDeleteSession()
+
+		itShouldReturnForbidden()
+	})
+
+	Context("when UAA responds with error param other than invalid_scope", func() {
+		BeforeEach(func() {
+			request.Form.Add("error", "unauthorized_client")
+		})
+
+		itShouldDeleteSession()
+
+		itShouldReturnInternalServerError()
+	})
+
+	Context("when state parameter in request is missing", func() {
+		BeforeEach(func() {
+			request.Form.Del("state")
+		})
+
+		itShouldDeleteSession()
+
+		itShouldReturnBadRequest()
+	})
+
+	Context("when a oauth code is missing in the request", func() {
+		BeforeEach(func() {
+			request.Form.Del("code")
+		})
+
+		itShouldDeleteSession()
+
+		itShouldReturnBadRequest()
+	})
+
+	Context("when original URL parameter is missing from session", func() {
+		BeforeEach(func() {
+			delete(session.Values, "targetUrl")
+		})
+
+		itShouldDeleteSession()
+
+		itShouldReturnBadRequest()
+	})
+
+	Context("when target URL parameter in session is not string", func() {
+		BeforeEach(func() {
+			session.Values["targetUrl"] = 1
+		})
+
+		itShouldDeleteSession()
+
+		itShouldReturnBadRequest()
+	})
+
+	Context("when state parameter is missing from session", func() {
+		BeforeEach(func() {
+			delete(session.Values, "state")
+		})
+
+		itShouldDeleteSession()
+
+		itShouldReturnBadRequest()
+	})
+
+	Context("when state parameter in session is not string", func() {
+		BeforeEach(func() {
+			session.Values["state"] = 1
+		})
+
+		itShouldDeleteSession()
+
+		itShouldReturnBadRequest()
+	})
+
+	Context("when state parameter in request does not equal the session one", func() {
+		BeforeEach(func() {
+			session.Values["state"] = "some_other_value"
+		})
+
+		itShouldDeleteSession()
+
+		itShouldReturnBadRequest()
+	})
+
+	Context("when provider fails to retrieve token", func() {
+		BeforeEach(func() {
+			tokenProvider.RequestTokenReturns(nil, errors.New("Could not get token"))
+		})
+
+		itShouldDeleteSession()
+
+		itShouldReturnInternalServerError()
+	})
+
+})
