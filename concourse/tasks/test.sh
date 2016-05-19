e72972bc5ff3e3044ec7dcaf4cf61eeb4b9a3b6f
Momchil Atanassov <momchil.atanassov@sap.com>
2016-05-19 16:31:36 +0300

    Remove unnecessary usage of dirname command

--- concourse/tasks/test.sh
+++ concourse/tasks/test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
 
 location=$GOPATH/src/github.infra.hana.ondemand.com/cloudfoundry/goauth_handlers
-
-mkdir -p $(dirname $location)
+mkdir -p $location
 cp -r goauth_handlers/. $location
-
 cd $location
 
 go get github.com/onsi/ginkgo/ginkgo
