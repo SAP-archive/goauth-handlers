58bc8dc2f49510e9fece529cb7d48a1c47a995bb
Momchil Atanassov <momchil.atanassov@sap.com>
2016-05-19 16:21:28 +0300

    Include hidden files when using copy command

--- concourse/tasks/test.sh
+++ concourse/tasks/test.sh
@@ -3,7 +3,7 @@
 location=$GOPATH/src/github.infra.hana.ondemand.com/cloudfoundry/goauth_handlers
 
 mkdir -p $(dirname $location)
-cp -r goauth_handlers/ $location
+cp -r goauth_handlers/. $location
 
 cd $location
 
